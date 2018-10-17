e1a59e5cb86a960daa216e4fb08a1065decaa9f4
dean-kim <keunu07@gmail.com>
2018-10-17 15:25:06 +0900

    update App component

--- dialogflow-test/src/App.js
+++ dialogflow-test/src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    handleClick() {
+        let text;
+
+        // make sure speech recognition is supported in user's browser
+        if (window['webkitSpeechRecognition']) {
+
+            const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+            const recognition = new SpeechRecognition();
+
+            // korean
+            recognition.lang = 'kor';
+
+            // process user audio while user is speaking
+            recognition.interimResults = false;
+
+            // speech recognition will not end when user stops speaking
+            recognition.continuous = false;
+
+            // start the speech --> string transcription process
+            recognition.start()
+
+            // this is what actually captures your speech & converts it to a str
+            recognition.onresult = e => {
+                let last = e.results.length - 1;
+
+                // text = what you just said, in string form
+                text = e.results[last][0].transcript;
+
+                // todo: will put a socket here later
+            }
+
+            // this will stop transcribing your speech when you stop speaking
+            recognition.onspeechend = () => {
+                recognition.stop();
+            }
+
+            // if error, set text = to what the error is so user knows
+            recognition.onerror = e => {
+                text = 'Error occurred in recognition: ' + e.error;
+                return;
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick}>test</button>
+            </div>
+        )
+    }
 }
 
 export default App;
