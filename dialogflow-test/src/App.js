189a4ebddad3c10fe39b398c587560e6fa7bff84
dean-kim <keunu07@gmail.com>
2018-10-19 09:00:28 +0900

    before yarn eject

--- dialogflow-test/src/App.js
+++ dialogflow-test/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import socket from 'socket.io'
+// import socket from 'socket.io'
+import openSocket from 'socket.io-client'
 import './App.css';
 
 
@@ -9,8 +10,30 @@ class App extends Component {
         says: ''
     }
 
+    // synthVoice = (AIResponse) => {
+    //
+    //     // create context for speech synthesis
+    //     const synth = window.speechSynthesis;
+    //     const AIStringAsVoice = new SpeechSynthesisUtterance();
+    //
+    //     // define what text AI will be speaking
+    //     AIStringAsVoice.text = text;
+    //
+    //     // customize AI's voice
+    //     // this is just a sampling of ways to customize voice
+    //     AIStringAsVoice.voiceURI = 'Native';
+    //     AIStringAsVoice.volume = 1;
+    //     AIStringAsVoice.rate = 1;
+    //     AIStringAsVoice.lang = 'ko-KR';
+    //
+    //     // this is what reads the string out loud
+    //     synth.speak(msg);
+    // }
+
     handleClick = () => {
         let text;
+
+        const socket = openSocket();
 
         // make sure speech recognition is supported in user's browser
         if (window['webkitSpeechRecognition']) {
@@ -30,25 +53,50 @@ class App extends Component {
             // start the speech --> string transcription process
             recognition.start();
 
+            console.log('inside if');
+
             // this is what actually captures your speech & converts it to a str
             recognition.onresult = e => {
                 let last = e.results.length - 1;
-                console.log('last', last);
 
                 // text = what you just said, in string form
                 text = e.results[last][0].transcript;
 
-                this.setState({says: text})
+                this.setState({says: text});
 
                 // socket
-                socket.emit("userAudio", text)
+                socket.emit("userAudio", text);
+
+                function synthVoice(AIResponse) {
+                    // CREATE CONTEXT FOR SPEECH SYNTHESIS
+                    const synth = window.speechSynthesis;
+                    const AIStringAsVoice = new SpeechSynthesisUtterance();
+                    // DEFINE WHAT TEXT AI WILL BE SPEAKING
+                    AIStringAsVoice.text = text;
+
+                    console.log('inside synthVoice')
+
+                    // CUSTOMIZE AI'S VOICE
+                    // THIS IS JUST A SAMPLING OF WAYS TO CUSTOMIZE VOICE
+                    AIStringAsVoice.voiceURI = 'Native';
+                    AIStringAsVoice.volume = 1;
+                    AIStringAsVoice.rate = 1;
+                    AIStringAsVoice.lang = 'ko-KR';
+                    // THIS IS WHAT READS THE STRING OUT LOUD
+                    synth.speak(text);
+                }
+
+                let AIResponse;
+
+                // socket.on("AIResponse", this.synthVoice(AIResponse))
+                socket.on('AIResponse', AIResponse);
+                this.synthVoice(AIResponse)
             }
 
             // this will stop transcribing your speech when you stop speaking
             recognition.onspeechend = () => {
                 recognition.stop();
             }
-            console.log(recognition);
 
             // if error, set text = to what the error is so user knows
             recognition.onerror = e => {
--- server.js
+++ server.js
@@ -1,22 +1,44 @@
-require('dotenv').config({ path: 'variables.env' });
+const secret = require('./secret.json');
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
+const http = require('http');
 const app = express();
+const server = http.createServer(app);
+server.listen(5000);
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const apiai = require('apiai')(secret["apiaiKey"]);
+const io = require('socket.io')(server);
+// const app = require('express')();
+// const server = require('http').createServer(app);
+// const io = require('socket.io')(server);
 
-app.post('/chat', (req, res) => {
-    const { message } = req.body;
-    console.log(message);
+io.on('connection', (socket) => {
+    socket.on("userAudio", text => {
+
+        // send text to AI here!
+        // capture what the user just said in a userTextToSendToAI
+        const userTextToSendToAI = text.text;
+
+        // send string to AI for processing
+        const AIReq = apiai.textRequest(userTextToSendToAI, {
+            sessionId: secret.sessionId
+        });
+
+        // wait for and capture response from AI
+        AIReq.on('response', (response) => {
+            const AIResponse = response.result.fulfillment.speech;
+
+            // use sockets to emit AIResponse to client here!
+            socket.emit('AIResponse', AIResponse)
+        });
+
+        AIReq.on('error', (error) => {
+            console.log(error)
+        });
+
+        AIReq.end();
+    });
 });
 
-app.set('port', process.env.PORT || 5000);
-const server = app.listen(app.get('post'), () => {
-    console.log(`Express running -> PORT ${server.address().pohl}`);
-});
+
 
